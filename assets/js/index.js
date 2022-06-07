cbc1610667ce0120e33b8f1dca409e2fd00b6aff
CrisArranz <c.arranz.arevalo@gmail.com>
2022-06-07 11:17:55 +0200

    Refactoring code and name of html

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,22 +14,26 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    let controls = document.querySelectorAll('.interface__actions-button');
+    const controls = document.querySelectorAll('.interface__actions-button');
     const isVisible = "is-visible";
-
-    controls.forEach((control) => {
-        control.addEventListener("click", () => {
-            if (control.innerText === 'CONTROLS') {
-                document.getElementById('boardModal').classList.add(isVisible);
-            }
-            if (control.innerText === 'START') {
-                document.querySelector('.no-visible').classList.add('visible');
-                document.querySelector('.no-visible').classList.remove('no-visible');
-            }
+    if (controls) {
+        controls.forEach((control) => {
+            control.addEventListener("click", () => {
+                if (control.innerText === 'CONTROLS') {
+                    document.getElementById('boardModal').classList.add(isVisible);
+                }
+                if (control.innerText === 'START') {
+                    document.querySelector('.no-visible').classList.add('visible');
+                    document.querySelector('.no-visible').classList.remove('no-visible');
+                }
+                if (control.innerText === '1 PLAYER') {
+                    location.href = "./selectedCharacter.html";
+                }
+            });
         });
-    })
+    }
 
-    const closeModal = document.querySelector("#boardModalClose");
+    const closeModal = document.getElementById("boardModalClose");
     if(closeModal){
         closeModal.addEventListener("click", () => {
             this.parentElement.parentElement.parentElement.classList.remove(isVisible);          
@@ -37,8 +41,18 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     document.addEventListener("click", e => {
-        if (e.target == document.querySelector(".modal.is-visible")) {
+        if (e.target === document.querySelector(".modal.is-visible")) {
             document.querySelector(".modal.is-visible").classList.remove(isVisible);
         }
     });
+
+    const characters = document.querySelectorAll(".section__interface-images");
+    if (characters) {
+        characters.forEach((character) => {
+            character.addEventListener('click', (event) => {
+                document.querySelector('.section__interface-banners-character').innerText = event.currentTarget.id.toUpperCase();
+            })
+        })
+    }
+
 });
